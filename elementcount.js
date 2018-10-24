66991ab5ac2397bc9240601f8538774de85b070f
Cari Palazzolo <cari.pizza@gmail.com>
2018-10-24 16:44:20 -0700

    updated js & html to return checked results in console.log

--- elementcount.js
+++ elementcount.js
@@ -2,25 +2,14 @@
 /* exported tryQuerySelector */
 'use strict';
 
-// eslint-disable-next-line
-var xIsTheNumber = 0;
+var elementsResponse = document.getElementById('elements-response');
 
 function tryQuerySelector() {
-    var elementsInputs = document.querySelectorAll('input[name="elements"]');
-
-    for(var i = 0; i < elementsInputs.length; i++) {
-        var input = elementsInputs[i];
-        console.log(input.value, input.id, input.checked);
+    var elements = document.querySelectorAll('input[name="elements"]:checked');
+    
+    for(var i = 0; i < elements.length; i++) {
+        var number = document.querySelectorAll(elements[i].value);
+        console.log(elements[i].value, number.length);
+        elementsResponse.textContent = 'You selected ' + [(elements[i].value, number.length)];
     }
-
-    var selectedElementsInput = document.querySelector('input[name="elements"]:checked');
-
-    if(selectedElementsInput) {
-        console.log(selectedElementsInput.value);
-    }
-    else {
-        console.log('no meal selected');
-    }
-
-    console.log(xIsTheNumber);
 }
