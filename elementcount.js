88347a552b379b6b62eaf461cf0cd47677f6f53b
Cari Palazzolo <cari.pizza@gmail.com>
2018-10-24 17:50:01 -0700

    change console.log to note and disabled in js file

--- elementcount.js
+++ elementcount.js
@@ -3,14 +3,13 @@
 'use strict';
 
 var elementsResponse = document.getElementById(['elements-response']);
-// var selectedElements = document.querySelectorAll('input[name="elements"]:checked');
 
 function tryQuerySelector() {
     var elements = document.querySelectorAll('input[name="elements"]:checked');
     
     for(var i = 0; i < elements.length; i++) {
         var number = document.querySelectorAll(elements[i].value);
-        console.log(elements[i].value, number.length);
+        // console.log(elements[i].value, number.length);
 
         elementsResponse.textContent = elementsResponse.textContent + 'You selected:\r\n' + [elements[i].value] + ', which appears ' + [number.length] + ' times.\r\n';
     }
